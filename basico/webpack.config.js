bb44c5323fdc3340b2d886fc081b37e1b1a633ec
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-01-19 10:38:04 +0100

    configure webpack development server, install html plugin

--- basico/webpack.config.js
+++ basico/webpack.config.js
@@ -1,3 +1,7 @@
+const path = require("node:path")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
 module.exports = {
     entry: ["./src/index.js"],
     module: {
@@ -9,4 +13,14 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html", // nombre del fichero en carpeta dist
+            template: "./src/index.html", // nombre de fichero que se usa como plantilla
+            scriptLoading: "blocking" // compatibilidad con navegadores antiguos
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, "./src")
+    }
 };
