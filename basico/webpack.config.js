7f258ba8df9fe20a556df38bbcf56e53cf6e65f4
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-01-19 11:15:18 +0100

    integrate css with webpack, install css loader and MiniCssExtractPlugin plugin

--- basico/webpack.config.js
+++ basico/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require("node:path")
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
 
 
 module.exports = {
-    entry: ["./src/index.js"],
+    entry: {
+        app: "./src/index.js"
+    },
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, "dist")
+    },
     module: {
         rules: [
             {
@@ -11,16 +20,29 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
             },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             filename: "index.html", // nombre del fichero en carpeta dist
             template: "./src/index.html", // nombre de fichero que se usa como plantilla
-            scriptLoading: "blocking" // compatibilidad con navegadores antiguos
+            scriptLoading: "blocking", // compatibilidad con navegadores antiguos
+            hash: true
+
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
         })
     ],
     devServer: {
-        static: path.join(__dirname, "./src")
+        static: path.join(__dirname, "./src"),
+        port: 8082
     }
 };
