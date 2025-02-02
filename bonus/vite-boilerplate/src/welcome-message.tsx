43341e60ea947c5d2905029b400364af2fe12fc5
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-02-02 16:09:12 +0100

    fixed logo image

--- bonus/vite-boilerplate/src/welcome-message.tsx
+++ bonus/vite-boilerplate/src/welcome-message.tsx
@@ -1,12 +1,13 @@
-import "./css/styles.scss"
-
 import React from "react";
+
+import "./css/styles.scss"
+import logo from './img/logo.png';
 
 export const WelcomeMessage: React.FC = () => {
     return (
         <>
             <div className="container">
-                <img src="./src/img/logo.png" alt="Vite Logo" className="logo" />
+                <img src={logo} alt="Vite Logo" className="logo" />
                 <h2>{import.meta.env.VITE_WELCOME_MESSAGE}</h2>
             </div>
         </>
